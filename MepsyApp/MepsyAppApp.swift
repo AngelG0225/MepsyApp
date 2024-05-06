6175607d9e799a476054912f50742611d8a6f623
AngelG0225 <AGuzman_7872@email.ric.edu>
2024-05-06 19:04:53 -0400

    Initial Commit

---
+++ MepsyApp/MepsyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MepsyAppApp.swift
+//  MepsyApp
+//
+//  Created by Angel Guzman on 5/6/24.
+//
+
+import SwiftUI
+
+@main
+struct MepsyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
